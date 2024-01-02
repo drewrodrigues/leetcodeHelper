b678deb165dff4e5bc0d1ad2f402466b5d4f9cae
Drew Rodrigues <rodandrew95@gmail.com>
2024-01-01 18:58:42 -0800

    Small fixes to make content policy happy from devepment enviornment

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  output: {
+    filename: "index.js",
+  },
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "manifest.json", to: "." }],

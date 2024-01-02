941790eb626cbb6bd2266b162d7a3f0f7048d7a3
Drew Rodrigues <rodandrew95@gmail.com>
2024-01-01 20:58:23 -0800

    Add style loading

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,28 @@ module.exports = {
   output: {
     filename: "index.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: "postcss-loader",
+          },
+        ],
+      },
+    ],
+  },
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "manifest.json", to: "." }],

f5ccb746ecac493b1c5c85b3f616790afc901e51
Drew Rodrigues <rodandrew95@gmail.com>
2024-01-11 14:08:35 -0800

    Handle light mode code editor styles for syntax highlighting

--- src/features/noSyntaxHighlighting.js
+++ src/features/noSyntaxHighlighting.js
@@ -1,7 +1,30 @@
 import { StyleHandler } from "../helpers/styleHandler";
 
+const PROBLEMS_CODE_EDITOR_LINE = ".monaco-scrollable-element span";
+const PROBLEMS_CODE_EDITOR_COMMENT = ".monaco-scrollable-element span.mtk3";
+
+// -----------------------------------------------------------------------------
+// Problems Page Code Editor (Light)
+// -----------------------------------------------------------------------------
+const PROBLEMS_CODE_EDITOR_LINE__LIGHT = `${PROBLEMS_CODE_EDITOR_LINE} { color: black !important; opacity: 1 !important; }`;
+const PROBLEMS_CODE_EDITOR_COMMENT__LIGHT = `${PROBLEMS_CODE_EDITOR_COMMENT} { opacity: 0.35 !important; }`;
+
+// -----------------------------------------------------------------------------
+// Problems Page Code Editor (Dark)
+// -----------------------------------------------------------------------------
+const VS_CODE_DARK_SELECTOR = ".vs-dark";
+const PROBLEMS_CODE_EDITOR_LINE__DARK = `${VS_CODE_DARK_SELECTOR} ${PROBLEMS_CODE_EDITOR_LINE} { color: white !important; opacity: 1 !important; }`;
+const PROBLEMS_CODE_EDITOR_COMMENT__DARK = `${VS_CODE_DARK_SELECTOR} ${PROBLEMS_CODE_EDITOR_COMMENT} { opacity: 0.35 !important; }`;
+
+// -----------------------------------------------------------------------------
+// Explore Page Code Editor
+// -----------------------------------------------------------------------------
+// TODO: make sure these are handled
+
 const disableSyntaxHighlighting = new StyleHandler([
-  ".monaco-scrollable-element span { color: white !important; opacity: 1 !important; }",
-  ".CodeMirror-line span { color: white !important; opacity: 1 !important; }",
+  PROBLEMS_CODE_EDITOR_LINE__LIGHT,
+  PROBLEMS_CODE_EDITOR_COMMENT__LIGHT,
+  PROBLEMS_CODE_EDITOR_LINE__DARK,
+  PROBLEMS_CODE_EDITOR_COMMENT__DARK,
 ]);
 export default disableSyntaxHighlighting;
